403487dda58863d34dcf7f7f5ef74b88814d47cf
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-09-19 08:46:44 +0530

    creating Feed

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Feed.css";
+import thumbnail1 from "../../assets/thumbnail1.png";
+import thumbnail2 from "../../assets/thumbnail2.png";
+import thumbnail3 from "../../assets/thumbnail3.png";
+import thumbnail4 from "../../assets/thumbnail4.png";
+import thumbnail5 from "../../assets/thumbnail5.png";
+import thumbnail6 from "../../assets/thumbnail6.png";
+import thumbnail7 from "../../assets/thumbnail7.png";
+import thumbnail8 from "../../assets/thumbnail8.png";
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <div className="card">
+        <img src={thumbnail1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import Feed from '../../components/Feed/Feed'
 
 const Home = ({sidebar}) => {
   return (
     <>
       <Sidebar sidebar={sidebar}/>
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed/>
+      </div>
     </>
   )
 }
