fc59babc1fc9f70876d09618068dfac4a12ed9a0
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-09-19 14:02:32 +0530

    creating and fetching youtube api

---
+++ src/data.js
@@ -0,0 +1,2 @@
+
+export const API_KEY = 'AIzaSyBSg_Hhl1jY6kJJz88MBVAqMk41_ZFPb0Y';
--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../components/PlayVideo/PlayVideo'
+import Recommended from '../../components/Recommended/Recommended'
 
 const Video = () => {
   return (
     <div className='play-container' >
       <PlayVideo/>
+      <Recommended/>
     </div>
   )
 }
