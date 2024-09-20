a0b0d0a102a963b0b0d550808e8fdd07890c0320
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-09-20 12:47:15 +0530

    deploy

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 
-// export const API_KEY = 'AIzaSyBSg_Hhl1jY6kJJz88MBVAqMk41_ZFPb0Y';
+export const API_KEY = 'AIzaSyBSg_Hhl1jY6kJJz88MBVAqMk41_ZFPb0Y';
 
 export const value_converter = (value)=> {
     if(value>1000000){
